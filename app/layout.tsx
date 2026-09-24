8fe3360be99de0d6861be87d23eb39e8119fe968
Lukman Mohammed <lukman728@gmail.com>
2026-09-24 03:13:55 +0000

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,27 +7,25 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-
   title: {
     default: "BEEBZ PRINTS",
     template: "%s | BEEBZ PRINTS",
   },
-
   description:
     "Professional printing and creative branding services for businesses, organizations, and individuals.",
-
   icons: {
     icon: "/api/favicon",
     shortcut: "/api/favicon",
     apple: "/api/favicon",
   },
+  verification: {
+    google: "_8UlcuTCoAHKN0ONCMj9h9QIXpwERzluS2YoCz7uvFs",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <head>
@@ -35,7 +33,6 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/api/favicon" />
         <link rel="apple-touch-icon" href="/api/favicon" />
       </head>
-
       <body>{children}</body>
     </html>
   );
